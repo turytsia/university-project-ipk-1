105988f44817ead06c5e661d255a53932432589b
turytsia <turytsia.alexander@gmail.com>
2023-02-22 00:43:18 +0100

    created ipkcpc.c

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	gcc -o ipkcpc ipkcpc.c
---
+++ ipkcpc.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/socket.h>
+
+#define ARGC 7
+
+#define OPTION_HOST "-h"
+#define OPTION_PORT "-p"
+#define OPTION_MODE "-m"
+
+#define COLOR_CYAN "\033[0;36m"
+#define COLOR_RED "\033[0;31m"
+#define COLOR_RESET "\033[0m"
+
+#define DEBUG 1
+
+void exit_with_message(const char *message, ...)
+{
+    va_list args;
+    va_start(args, message);
+    fprintf(stderr, COLOR_RED "[ERROR]" COLOR_RESET);
+    vfprintf(stderr,message, args);
+    printf("\n");
+    va_end(args);
+    exit(EXIT_FAILURE);
+}
+
+void debug(const char*message, ...){
+    if(!DEBUG)
+        return;
+    va_list args;
+    va_start(args, message);
+    printf(COLOR_CYAN "[DEBUG]" COLOR_RESET);
+    vprintf(message, args);
+    printf("\n");
+    va_end(args);
+}
+
+int main(int argc, char**argv) {
+    if (argc != ARGC)
+        exit_with_message("Usage %s -h <host> -p <port> -m <mode>", argv[0]);
+
+    char host[255];
+    char port[255];
+    char mode[255];
+
+    debug("Reading arguments...");
+    for (int i = 1; i < ARGC; i += 2)
+    {
+        if (strcmp(argv[i], OPTION_HOST) == 0){
+            debug("host: %s", argv[i + 1]);
+            strcpy(host, argv[i + 1]);
+        }
+        else if (strcmp(argv[i], OPTION_PORT) == 0){
+            debug("port: %s", argv[i + 1]);
+            strcpy(port, argv[i + 1]);
+        }
+        else if (strcmp(argv[i], OPTION_MODE) == 0){
+            debug("mode: %s", argv[i + 1]);
+            strcpy(mode, argv[i + 1]);
+        }
+        else{
+            exit_with_message("Unknown command %s", argv[i]);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}

8445de7168b899056706d0ece7e3e3e114fd26ad
turytsia <italexx.cz@gmail.com>
2023-02-22 08:36:39 +0100

    added connection

--- ipkcpc.c
+++ ipkcpc.c
@@ -16,19 +16,18 @@
 
 #define DEBUG 1
 
-void exit_with_message(const char *message, ...)
-{
+void exit_with_message(const char* message, ...) {
     va_list args;
     va_start(args, message);
     fprintf(stderr, COLOR_RED "[ERROR]" COLOR_RESET);
-    vfprintf(stderr,message, args);
+    vfprintf(stderr, message, args);
     printf("\n");
     va_end(args);
     exit(EXIT_FAILURE);
 }
 
-void debug(const char*message, ...){
-    if(!DEBUG)
+void debug(const char* message, ...) {
+    if (!DEBUG)
         return;
     va_list args;
     va_start(args, message);
@@ -38,7 +37,7 @@ void debug(const char*message, ...){
     va_end(args);
 }
 
-int main(int argc, char**argv) {
+int main(int argc, char** argv) {
     if (argc != ARGC)
         exit_with_message("Usage %s -h <host> -p <port> -m <mode>", argv[0]);
 
@@ -47,24 +46,48 @@ int main(int argc, char**argv) {
     char mode[255];
 
     debug("Reading arguments...");
-    for (int i = 1; i < ARGC; i += 2)
-    {
-        if (strcmp(argv[i], OPTION_HOST) == 0){
+    for (int i = 1; i < ARGC; i += 2) {
+        if (strcmp(argv[i], OPTION_HOST) == 0) {
             debug("host: %s", argv[i + 1]);
             strcpy(host, argv[i + 1]);
         }
-        else if (strcmp(argv[i], OPTION_PORT) == 0){
+        else if (strcmp(argv[i], OPTION_PORT) == 0) {
             debug("port: %s", argv[i + 1]);
             strcpy(port, argv[i + 1]);
         }
-        else if (strcmp(argv[i], OPTION_MODE) == 0){
+        else if (strcmp(argv[i], OPTION_MODE) == 0) {
             debug("mode: %s", argv[i + 1]);
             strcpy(mode, argv[i + 1]);
         }
-        else{
+        else {
             exit_with_message("Unknown command %s", argv[i]);
         }
     }
+
+    debug("Opening socket...");
+    int socket_desc;
+    if (strcmp(mode,"tcp") == 0) {
+        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    else if (strcmp(mode, "udp") == 0) {
+        socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
+    }
+    else {
+        exit_with_message("Unknown mode: %s",mode);
+    }
+    debug("socket_desc: %d", socket_desc);
+
+    if (socket_desc < 0)
+        exit_with_message("Unable to create socket.");
+
+    struct sockaddr_in server;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    // if (inet_pton(AF_INET, host.c_str(), &serv_addr.sin_addr) <= 0) {
+    //     error("Invalid server address");
+    // }
+
 
     return EXIT_SUCCESS;
 }
